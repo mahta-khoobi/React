8e3bdba0398763dc16893dec3876cc2313ed69c1
khoobim <mahta.khoobi@adesso.de>
2022-04-24 20:17:11 +0200

    MenuComponent added with Card implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
